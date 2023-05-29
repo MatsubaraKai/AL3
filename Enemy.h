684169c89eb12d543b19f67b92619fcdb1be75b8
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-05-29 13:43:22 +0900

    02_07a確認課題

--- Enemy.h
+++ Enemy.h
@@ -3,10 +3,10 @@
 #include "WorldTransform.h"
 
 enum class Phase {
-   Approach,//接近
-   Leave,//離脱
-
+	Approach,
+	Leave,
 };
+
 class Enemy {
 public:
 	void Initialize(Model* model, const Vector3& position);
@@ -15,11 +15,26 @@ public:
 
 	void Draw(const ViewProjection& viewProjection);
 
+	// 自作メンバ関数
+	void Approach();
+
+	void Leave();
+
 private:
+	// メンバ関数ポインタのテーブル
+	static void (Enemy::*phaseTable_[])();
+
 	WorldTransform worldTransform_;
 
 	Model* model_ = nullptr;
 
 	uint32_t textureHandle_ = 0u;
+
+	Vector3 move_;
+
+	float kApproachSpeed_;
+
+	float kLeaveSpeed_;
+
 	Phase phase_ = Phase::Approach;
 };
