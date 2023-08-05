6effc7c6573746ba2203d16999937fc0caea897b
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-08-05 22:43:19 +0900

    02_13

--- Enemy.cpp
+++ Enemy.cpp
@@ -2,21 +2,19 @@
 #include "Matrix.h"
 #include "Player.h"
 #include <cassert>
+#include "GameScene.h"
 
 Enemy::Enemy() {}
 
-Enemy::~Enemy() {
-
-	for (EnemyBullet* bullet : bullets_) {
-		delete bullet;
-	}
-
-	for (TimedCall* timedCall : timedCalls_) {
+Enemy::~Enemy() 
+{
+	for (TimedCall* timedCall : timedCalls_)
+	{
 		delete timedCall;
 	}
 }
 
-void Enemy::Initialize(Model* model, Vector3 pos) {
+void Enemy::Initialize(Model* model, const Vector3& pos, const Vector3& velocity) {
 	assert(model);
 
 	model_ = model;
@@ -29,53 +27,51 @@ void Enemy::Initialize(Model* model, Vector3 pos) {
 
 	worldTransform_.Initialize();
 
+	velocity_ = velocity;
+
 	worldTransform_.translation_ = pos;
 
 	FireTimer_ = kFireInterval;
 	FireandReset();
 }
 
-void Enemy::Update() {
-	// デスフラグの立った弾の削除
-	bullets_.remove_if([](EnemyBullet* bullet) {
-		if (bullet->IsDead()) {
-			delete bullet;
-			return true;
-		}
-		return false;
-	});
-
-	phase_->Update(this);
+void Enemy::Update() 
+{
+	 phase_->Update(this);
 
 	// タイマー
-	timedCalls_.remove_if([](TimedCall* timedcall) {
-		if (timedcall->IsFinish()) {
+	timedCalls_.remove_if([](TimedCall* timedcall) 
+		{
+		if (timedcall->IsFinish())
+		{
 			delete timedcall;
 			return true;
 		}
 		return false;
 	});
-	for (TimedCall* timedCall : timedCalls_) {
+
+	for (TimedCall* timedCall : timedCalls_)
+	{
 		timedCall->Update();
 	}
 
 	// ワールドトランスフォームの更新
 	worldTransform_.UpdateMatrix();
-
-	// 弾更新
-	for (EnemyBullet* bullet : bullets_) {
-		bullet->Update();
-	}
 }
 
-void Enemy::ChangePhase(EnemyState* newState) {
+void Enemy::ChangePhase(EnemyState* newState)
+{
 	delete phase_;
 	phase_ = newState;
 }
 
-void Enemy::Move(Vector3 speed) { worldTransform_.translation_ += speed; };
+void Enemy::Move(Vector3 speed)
+{
+	worldTransform_.translation_ += speed; 
+};
 
-void Enemy::Fire() {
+void Enemy::Fire() 
+{
 	assert(player_);
 
 	// 弾の速度
@@ -95,22 +91,22 @@ void Enemy::Fire() {
 	newBullet->Initialize(model_, worldTransform_.translation_, velocity);
 
 	// 弾を登録する
-	bullets_.push_back(newBullet);
+	gameScene_->AddEnemyBullet(newBullet);
 }
 
-void Enemy::OnCollision() {}
+void Enemy::OnCollision() 
+{
+	isDead_ = true; 
+}
 
-void Enemy::Draw(const ViewProjection& viewProjection) {
+void Enemy::Draw(const ViewProjection& viewProjection)
+{
 	// モデルの描画
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
-
-	// 弾描画
-	for (EnemyBullet* bullet : bullets_) {
-		bullet->Draw(viewProjection);
-	}
 }
 
-void Enemy::FireandReset() {
+void Enemy::FireandReset() 
+{
 	// 攻撃処理
 	Fire();
 
@@ -118,7 +114,8 @@ void Enemy::FireandReset() {
 	timedCalls_.push_back(new TimedCall(std::bind(&Enemy::FireandReset, this), kFireInterval));
 }
 
-Vector3 Enemy::GetWorldPosition() {
+Vector3 Enemy::GetWorldPosition() 
+{
 	Vector3 worldPos;
 	worldPos.x = worldTransform_.matWorld_.m[3][0];
 	worldPos.y = worldTransform_.matWorld_.m[3][1];
