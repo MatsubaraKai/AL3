d66b5282c7323ef22da593636de272fc51f82263
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-08-31 22:05:33 +0900

    名前変更

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2B_06_オオハシ_ヒロキ_AL3");
+	win->CreateGameWindow(L"二年後期ゲーム制作β版");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
